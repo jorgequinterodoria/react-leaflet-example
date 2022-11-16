de1b700566e18c92cacbb9886b8de9174acaced9
jquintedori <jquintedori@gmail.com>
2022-11-16 11:02:29 -0500

    Añadido logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import './Home.css'
-import fondo from "../assets/fondo.png"
+import fondo from "../assets/fondob.png"
+import logo from "../assets/fondo.png"
 
 const Home = () => {
   const [state, setState] = useState({
@@ -28,35 +29,38 @@ const Home = () => {
   }, []);
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      width: '100vw',
-      height: '100vh',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundImage: `url(${fondo})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover'
-    }}>
-      <h1 style={{ color: 'white', background: 'rgb(51 181 229 / 40%)' }}>Domicilios Brioche</h1><br />
-      <p style={{ color: 'white', background: 'rgb(51 181 229 / 40%)' }}>Tu Ubicación:</p><br />
-      <p style={{ color: 'white', background: 'rgb(51 181 229 / 40%)' }}>Latitud: {state.latitude}</p><br />
-      <p style={{ color: 'white', background: 'rgb(51 181 229 / 40%)' }}>longitud: {state.longitude}</p><br />
+    <>
+      <div>
+        <img src={logo} style={{ width: '400px', height: '267px' }} />
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100vw',
+        height: '60vh',
+        alignItems: 'center',
+        backgroundImage: `url(${fondo})`,
+        backgroundRepeat: 'no-repeat',
+      }}>
+        <h1 >Domicilios Brioche</h1><br />
+        <p >Tu Ubicación:</p><br />
+        <p >Latitud: {state.latitude}</p><br />
+        <p >longitud: {state.longitude}</p><br />
 
-      <Link
-        to={{
-          pathname: "/map",
-          // state: {
-          //   hello: 'world'
-          // }
-          state,
-        }}
-        style={{ background: 'rgb(51 181 229 / 40%)' }}
-      >
-        Ver Marcadores
-      </Link>
-    </div>
+        <Link
+          to={{
+            pathname: "/map",
+            // state: {
+            //   hello: 'world'
+            // }
+            state,
+          }}
+          style={{ background: 'rgb(51 181 229 / 40%)' }}
+        >
+          Ver Marcadores
+        </Link>
+      </div>
+    </>
   );
 };
 
