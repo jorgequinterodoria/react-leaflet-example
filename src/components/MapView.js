d00b9ac5186b2b683c816f8918a4c7583aa46dba
jorgequinterodoria <jquintedori@gmail.com>
2023-05-05 16:18:08 -0500

    Agregado Mayo

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Map, TileLayer, LayersControl, LayerGroup } from "react-leaflet";
 import data from "../assets/dataNov.json";
 import dataDic from "../assets/dataDic.json";
-import { VenueMarkersDic, VenueMarkersNov } from "./VenueMarkers";
-
+import dataMay from "../assets/dataMayo.json";
+import {
+  VenueMarkersDic,
+  VenueMarkersNov,
+  VenueMarkersMay,
+} from "./VenueMarkers";
 import { useLocation, useHistory } from "react-router-dom";
 
 import "leaflet/dist/leaflet.css";
@@ -19,6 +23,11 @@ const MapView = (props) => {
     zoom: 13,
     dataDic,
   });
+  const [stateMay, setStateMay] = useState({
+    currentLocation: { lat: 8.7705699, lng: -75.8652575 },
+    zoom: 13,
+    dataMay,
+  });
 
   const location = useLocation();
   const history = useHistory();
@@ -29,7 +38,6 @@ const MapView = (props) => {
         lat: location.state.latitude,
         lng: location.state.longitude,
       };
-      console.log("State diciembre", stateDic);
       setState({
         ...state,
         data: {
@@ -50,6 +58,16 @@ const MapView = (props) => {
         },
         currentLocation,
       });
+      setStateMay({
+        ...stateMay,
+        dataMay: {
+          venues: stateMay.dataMay.venues.concat({
+            name: "Mi ubicación",
+            geometry: [currentLocation.lat, currentLocation.lng],
+          }),
+        },
+        currentLocation,
+      });
       history.replace({
         pathname: "/map",
         state: {},
@@ -59,27 +77,29 @@ const MapView = (props) => {
   }, [location]);
 
   return (
-    <>
-      {console.log("State diciembre", stateDic)}
-      <Map center={state.currentLocation} zoom={state.zoom}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <LayersControl position="topright">
-          <LayersControl.Overlay name="Noviembre">
-            <LayerGroup>
-              <VenueMarkersNov venues={state.data.venues} />
-            </LayerGroup>
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="Diciembre">
-            <LayerGroup>
-              <VenueMarkersDic venues={stateDic.dataDic.venues} />
-            </LayerGroup>
-          </LayersControl.Overlay>
-        </LayersControl>
-      </Map>
-    </>
+    <Map center={state.currentLocation} zoom={state.zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Noviembre">
+          <LayerGroup>
+            <VenueMarkersNov venues={state.data.venues} />
+          </LayerGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Diciembre">
+          <LayerGroup>
+            <VenueMarkersDic venues={stateDic.dataDic.venues} />
+          </LayerGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Mayo - 2023">
+          <LayerGroup>
+            <VenueMarkersMay venues={stateMay.dataMay.venues} />
+          </LayerGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
+    </Map>
   );
 };
 
